21698384712f8c76f4874ba3130ef58a5ee449dd
Onur Kenis <kenisonur@yahoo.com>
2016-11-09 22:33:11 +0300

    Final

---
+++ src/main/java/edu/bim313/App.java
@@ -0,0 +1,33 @@
+package edu.bim313;
+import org.paukov.combinatorics3.Generator;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by onur on 09.11.2016.
+ */
+
+public class App
+{
+    public static void main( String[] args )
+    {
+
+        ArrayList<Integer> cityList = new ArrayList<>();
+        List<Triangle> triangleList= new ArrayList<>();
+
+        for(int i=0;i<81;i++){
+            cityList.add(i);
+        }
+
+        Generator.combination(cityList)
+                .simple(3)
+                .stream()
+                .forEach((t)->triangleList.add(new Triangle(t)));
+
+        triangleList
+                .stream()
+                .filter(((t) ->t.checkInEquality() ))
+                .forEach(System.out::println);
+
+    }
+}
